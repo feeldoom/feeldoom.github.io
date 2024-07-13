54fa9e529e31051327de32c7415b23418990a359
husarenko <64213424+feeldoom@users.noreply.github.com>
2024-07-13 18:01:56 +0300

    fc

---
+++ src/js/scripts.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', (_event) => {
+    function updateTime() {
+        const now = new Date();
+        const hours = now.getHours().toString().padStart(2, '0');
+        const minutes = now.getMinutes().toString().padStart(2, '0');
+        const timeString = `${hours}:${minutes}`;
+        document.getElementById('current-time').textContent = timeString;
+    }
+
+    setInterval(updateTime, 5000);
+    updateTime();
+
+    function updateNews() {
+        const newsTicker = document.getElementById('news-ticker');
+        const newsItems = [
+            "[07/13/24] Website created"
+        ];
+        let newsIndex = 0;
+
+        function showNextNews() {
+            newsTicker.textContent = newsItems[newsIndex];
+            newsIndex = (newsIndex + 1) % newsItems.length;
+        }
+
+        setInterval(showNextNews, 5000);
+        showNextNews();
+    }
+
+    updateNews();
+});
